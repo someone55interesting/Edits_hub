627a8f5b0db6171e33bff47d13d74ee1b9f5eb07
Magomed <magomed@MacBook-Pro-Magomed.local>
2026-02-20 23:14:58 +0300

    Full fix for FFmpeg and Cloudinary

--- edits/admin.py
+++ edits/admin.py
@@ -7,7 +7,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Edit)
 class EditAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'category', 'views', 'created_at')
+    list_display = ('title', 'author', 'category', 'views_count', 'created_at')
     list_filter = ('category', 'created_at')
     search_fields = ('title', 'description', 'author__username')
-    readonly_fields = ('views',)
+    readonly_fields = ('views_count',)
